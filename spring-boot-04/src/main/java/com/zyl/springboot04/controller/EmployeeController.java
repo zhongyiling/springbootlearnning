cc925f062eb8d2249536e92bac188149c113b517
zhongyiling <15094084328@163.com>
2019-09-23 21:31:48 +0800

    修改员工和删除员工
    1.修改员工
    2.删除员工

--- spring-boot-04/src/main/java/com/zyl/springboot04/controller/EmployeeController.java
+++ spring-boot-04/src/main/java/com/zyl/springboot04/controller/EmployeeController.java
@@ -7,7 +7,7 @@ import com.zyl.springboot04.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
@@ -36,5 +36,41 @@ public class EmployeeController {
         Collection<Department> departments = departmentDao.getDepartments();
         model.addAttribute("depts",departments);
         return "emp/add";
+    }
+
+    //员工添加
+    //SpringMVC自动将请求参数和入参对象的属性进行一一绑定；要求请求参数的名字和javaBean入参的对象里面的属性名是一样的
+    @PostMapping("/emp")
+    public String addEmployee(Employee employee){
+        //来到员工列表页面
+        System.out.println("保存的员工信息："+employee);
+        // redirect: 表示重定向到一个地址  /代表当前项目路径
+        // forward: 表示转发到一个地址
+        employeeDao.save(employee);
+        return "redirect:/emps";
+    }
+    @GetMapping("/emp/{id}")
+    public String toEditPage(@PathVariable("id") Integer id,Model model){
+        Employee employee = employeeDao.get(id);
+        model.addAttribute("emp",employee);
+        Collection<Department> departments = departmentDao.getDepartments();
+        System.out.println(departments);
+        model.addAttribute("depts",departments);
+        //回到修改页面(add是一个修改添加二合一的页面);
+        return "emp/add";
+    }
+
+    @PutMapping("/emp")
+    //员工修改；需要提交员工id；
+    public String updateEmployee(Employee employee){
+        System.out.println(employee);
+        employeeDao.save(employee);
+        return "redirect:/emps";
+    }
+
+    @DeleteMapping("/emp/{id}")
+    public String deleteEmployee(@PathVariable("id") Integer id){
+        employeeDao.delete(id);
+        return "redirect:/emps";
     }
 }
